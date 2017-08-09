51d0a2824feeb55962df7c8564b2e728680ef060
Petros G. Sideris <petros.sideris@growthstreet.co.uk>
2017-08-09 19:54:56 +0100

    add animation

--- app/src/main/java/xyz/pgsideris/pomodoro/MainActivity.kt
+++ app/src/main/java/xyz/pgsideris/pomodoro/MainActivity.kt
@@ -9,18 +9,23 @@ import android.widget.Chronometer
 import kotlinx.android.synthetic.main.activity_main.*
 import android.os.Vibrator
 import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.view.animation.*
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : WearableActivity() {
     var timerRunning: Boolean = false
     var timeWhenStopped: Long = 0
-    val minutes: Int = 25
+    var minutes: Int = 25
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // Enables Always-on
         setAmbientEnabled()
+        this.durationDisplay.text = minutesToFormat(minutes)
         this.chronos.onChronometerTickListener = Chronometer.OnChronometerTickListener {
             cArg ->
                 val t = SystemClock.elapsedRealtime() - cArg.base
@@ -43,10 +48,31 @@ class MainActivity : WearableActivity() {
         }
     }
 
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        when (keyCode) {
+            KeyEvent.KEYCODE_NAVIGATE_NEXT -> {
+                minutes += 5
+                return true
+            }
+            KeyEvent.KEYCODE_NAVIGATE_PREVIOUS -> {
+                if(minutes > 5) {
+                    minutes -= 5
+                    return true
+                }
+                return false
+
+            }
+        }
+        this.durationDisplay.text = minutesToFormat(minutes)
+        return super.onKeyDown(keyCode, event)
+    }
+
     fun timerStart() {
         timerRunning = true
         this.chronos.base = SystemClock.elapsedRealtime() + timeWhenStopped
         this.chronos.start()
+        this.durationDisplay.visibility = View.VISIBLE
+        replace(0, -20)
     }
 
     fun timerPause() {
@@ -75,6 +101,29 @@ class MainActivity : WearableActivity() {
         //-1 - don't repeat
         val indexInPatternToRepeat = -1
         vibrator.vibrate(vibrationPattern, indexInPatternToRepeat)
+    }
+
+    fun minutesToFormat(minutes: Int) : String {
+        val time = TimeUnit.MINUTES.toMillis(minutes.toLong())
+        return DateFormat.format("mm:ss", time).toString()
+    }
+
+    fun replace(xTo: Int, yTo: Int) {
+        // create set of animations
+        val replaceAnimation = AnimationSet(false)
+        replaceAnimation.interpolator = DecelerateInterpolator()
+        // animations should be applied on the finish line
+        replaceAnimation.fillAfter = true
+
+        val translation = TranslateAnimation(0, 0f, TranslateAnimation.ABSOLUTE, (xTo - this.durationDisplay.left).toFloat(),
+                0, 0f, TranslateAnimation.ABSOLUTE, (yTo - this.durationDisplay.right).toFloat())
+        translation.duration = 1000
+        val alpha = AlphaAnimation(1f, 0f)
+        alpha.duration = 500
+        replaceAnimation.addAnimation(translation)
+        replaceAnimation.addAnimation(alpha)
+        this.durationDisplay.animation = replaceAnimation
+        this.durationDisplay.animate()
     }
 
 
