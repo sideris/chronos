b8e0f917dcd3b4921b6fea6c5df4c4498786f7da
Petros G. Sideris <petros.sideris@growthstreet.co.uk>
2017-08-09 02:46:25 +0100

    add reset and limit

--- app/src/main/java/xyz/pgsideris/pomodoro/MainActivity.kt
+++ app/src/main/java/xyz/pgsideris/pomodoro/MainActivity.kt
@@ -8,12 +8,13 @@ import android.text.format.DateFormat
 import android.widget.Chronometer
 import kotlinx.android.synthetic.main.activity_main.*
 import android.os.Vibrator
-
+import android.util.Log
 
 
 class MainActivity : WearableActivity() {
     var timerRunning = false
-    var timeWhenStopped = 0
+    var timeWhenStopped: Long = 0
+    val minutes = 25
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +23,13 @@ class MainActivity : WearableActivity() {
         setAmbientEnabled()
         this.chronos.onChronometerTickListener = Chronometer.OnChronometerTickListener {
             cArg ->
-                cArg.format = "mm:ss"
                 val t = SystemClock.elapsedRealtime() - cArg.base
+                val slices = getTimeFromMillis(t)
                 cArg.text = DateFormat.format("mm:ss", t)
+                if (slices[1] == minutes && slices[2] == 0) {
+                    timerPause()
+                    vibrate()
+                }
         }
 
         this.container.setOnClickListener {
@@ -32,21 +37,36 @@ class MainActivity : WearableActivity() {
             else                timerStart()
             timerRunning = !timerRunning
         }
+        this.container.setOnLongClickListener {
+            timerPause()
+            timerReset()
+            true
+        }
     }
 
     fun timerStart() {
-        this.chronos.base = SystemClock.elapsedRealtime() + timeWhenStopped;
+        this.chronos.base = SystemClock.elapsedRealtime() + timeWhenStopped
         this.chronos.start()
     }
 
     fun timerPause() {
         this.chronos.stop()
-        timeWhenStopped = (this.chronos.base - SystemClock.elapsedRealtime()).toInt()
+        timeWhenStopped = this.chronos.base - SystemClock.elapsedRealtime()
     }
 
     fun timerReset() {
-        this.chronos.base = SystemClock.elapsedRealtime();
+        this.chronos.base = SystemClock.elapsedRealtime()
+        this.chronos.text = "00:00"
         timeWhenStopped = 0
+        timerRunning = false
+    }
+
+    fun getTimeFromMillis(millis: Long): IntArray {
+        val hours = millis / 3600000
+        val minutes = (millis - hours * 3600000) / 60000
+        val seconds = (millis - hours * 3600000 - minutes * 60000) / 1000
+        val results: IntArray = intArrayOf(hours.toInt(), minutes.toInt(), seconds.toInt())
+        return results
     }
 
     fun vibrate() {
